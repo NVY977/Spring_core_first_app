61101aa72b6801cfa56dc2f107415df85961aa37
NVY977 <nvy977@gmail.com>
2022-06-01 13:36:32 +0300

    IoC, Dependency Ijection and Scope Bean are done

---
+++ src/main/java/ru/nvy/spring/ClassicalMusic.java
@@ -0,0 +1,11 @@
+package ru.nvy.spring;
+
+/**
+ * @author NVY
+ */
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Rhapsody";
+    }
+}
---
+++ src/main/java/ru/nvy/spring/RockMusic.java
@@ -0,0 +1,11 @@
+package ru.nvy.spring;
+
+/**
+ * @author NVY
+ */
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "In Bloom";
+    }
+}
